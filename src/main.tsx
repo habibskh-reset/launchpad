72816ca5f73473362b7e285c28adead980c93045
habibskh-reset <habib.skh@gmail.com>
2026-09-10 22:47:36 +0530

    fix: remove orphaned SecurityLockOverlay import from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { registerSW } from "virtual:pwa-register";
-import { ThemeProvider } from "@/components/auth/ThemeToggle";
-import { LoginOverlay } from "@/components/auth/LoginOverlay";
-import { SecurityLockOverlay } from "@/components/auth/SecurityLockOverlay";
-import { ErrorBoundary } from "@/components/shared/ErrorBoundary";
-import { router } from "@/router";
+import { router } from "./router";
 import "./index.css";
 
-registerSW({ immediate: true });
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <LoginOverlay />
-        <SecurityLockOverlay />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
